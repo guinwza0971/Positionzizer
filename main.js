9791326c3c42b2607030a00d03652cfc67ae4d4a
Rabbit hodl <101968638+bkminer1984@users.noreply.github.com>
2022-09-29 11:33:09 +0700

    update to current version

---
+++ main.js
@@ -0,0 +1,34 @@
+function calculatepositionsize() {
+    let cash = document.getElementById("totalcash").value
+    let risk = document.getElementById("riskpercent").value
+    let leverage = document.getElementById("leverage").value
+    let entryprice = document.getElementById("entryprice").value
+    let exitprice = document.getElementById("exitprice").value
+    let percentchange = document.getElementById("percentchange").value
+    let mode
+
+    const radiolist = document.getElementsByName("stoptype")
+    radiolist.forEach(element => {
+        if(element.checked == true)
+        {
+            mode = element.value
+        }
+    });
+    
+    let calculatedresult
+
+    if(leverage <= 1)
+        leverage = leverage+1
+
+    if(mode =="useprice")
+    {
+        calculatedresult = (cash * risk) / ((leverage - 1) * Math.abs(entryprice - exitprice)/entryprice*100)
+    }
+
+    if(mode =="usepercent")
+    {
+        calculatedresult = (cash * risk) / ((leverage - 1) * percentchange)
+    }
+
+    document.getElementById("result").innerHTML = calculatedresult;
+}
