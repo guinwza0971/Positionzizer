728dd4c34aad37beee7ebf608776de45b9a40b54
Jarnmel Suphakijudomkarn <64186635+guinwza0971@users.noreply.github.com>
2023-01-06 20:14:51 +0700

    Update main.js
    Add contact size, fund at risk

--- main.js
+++ main.js
@@ -16,16 +16,24 @@ function calculatepositionsize() {
     });
     
     let calculatedresult
+    let contactsize
+    let moneytolose
 
     if(mode =="useprice")
     {
         calculatedresult = (cash * risk) / ((leverage) * Math.abs(entryprice - exitprice)/entryprice*100)
+        contactsize = (cash * risk/100) / Math.abs(entryprice - exitprice)
+        moneytolose = cash * risk/100
     }
 
     if(mode =="usepercent")
     {
         calculatedresult = (cash * risk) / ((leverage) * percentchange)
+        contactsize = (cash * risk/100) / Math.abs(entryprice - (((100 - percentchange)*entryprice))/100)
+        moneytolose = cash * risk/100
     }
 
     document.getElementById("result").innerHTML = calculatedresult;
+    document.getElementById("contacts").innerHTML = contactsize;
+    document.getElementById("fundatrisk").innerHTML = moneytolose;
 }
